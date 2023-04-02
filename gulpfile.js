67e1f1007bf155e4fa841707370baf037f061773
Colonel Blimp <48870400+colonel-blimp@users.noreply.github.com>
2023-04-02 18:03:47 -0400

    Extra gulp things (not 100%)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
+const argv = require('yargs')
+const spawn = require('child_process').spawn;
 
 gulp.task('pug', function() {
   return gulp.src('src/**/*.pug')
@@ -32,7 +34,25 @@ gulp.task('watch', function() {
   gulp.watch('src/**/*.pug', gulp.series('pug'));
   gulp.watch('src/**/*.scss', gulp.series('scss'));
   gulp.watch('src/**/*.js', gulp.series('js'));
+  let process;
+
+  const restart = () => {
+    if (process) {
+      process.kill();
+    }
+    console.log(`argv: ${argv}`)
+    process = spawn('gulp', [argv.task], {stdio: 'inherit'});
+  };
+
+  // Watch the gulpfile and restart the process if it changes
+  gulp.watch('gulpfile.js', () => {
+    restart();
+  });
 });
 
+gulp.task('moveFavicon', function() {
+  return gulp.src('src/assets/favicon.ico')
+    .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('default', gulp.parallel('pug', 'scss', 'js', 'watch'));
