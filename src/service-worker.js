2ceeaa1d438ddade76b4f386debada2cd865cda6
Colonel Blimp <48870400+colonel-blimp@users.noreply.github.com>
2023-04-09 00:05:30 -0400

    More service worker workery

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,22 +1,24 @@
+const BASE_URL = '/m8-bpm-pitch-calculator';
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('m8-bpm-pit-calc--cache').then(function(cache) {
         return cache.addAll([
-          '/m8-bpm-pitch-calculator/',
-          '/m8-bpm-pitch-calculator/index.html',
-          '/m8-bpm-pitch-calculator/styles/styles.css',
-          '/m8-bpm-pitch-calculator/js/app.js',
-          '/m8-bpm-pitch-calculator/js/hex_dec_converters.js',
-          '/m8-bpm-pitch-calculator/js/service-worker.js'
+          `${BASE_URL}/`,
+          `${BASE_URL}/index.html`,
+          `${BASE_URL}/styles/styles.css`,
+          `${BASE_URL}/js/app.js`,
+          `${BASE_URL}/js/hex_dec_converters.js`,
+          `${BASE_URL}/js/service-worker.js`
         ]);
       })
     );
   });
   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
